b55e557c7700adbb80f6627a57004f7cbc75e464
Hendrik Albers <mail@hendrik.dev>
2020-08-12 23:55:22 +0200

    rename variable classes -> classes2da

--- src/creature.nim
+++ src/creature.nim
@@ -19,12 +19,12 @@ type
     class1, class2, class3: string
     level1, level2, level3: int
 
-proc classes*(classList: GffList, classes: TwoDA, dlg: SingleTlk, tlk: Option[SingleTlk]): Classes =
-  result.class1 = classes[classList[0]["Class", GffInt], "Name"].get.parseInt.StrRef.tlkText(dlg, tlk)
+proc classes*(classList: GffList, classes2da: TwoDA, dlg: SingleTlk, tlk: Option[SingleTlk]): Classes =
+  result.class1 = classes2da[classList[0]["Class", GffInt], "Name"].get.parseInt.StrRef.tlkText(dlg, tlk)
   result.level1 = classList[0]["ClassLevel", GffShort]
   if classList.len >= 2:
-    result.class2 = classes[classList[1]["Class", GffInt], "Name"].get.parseInt.StrRef.tlkText(dlg, tlk)
+    result.class2 = classes2da[classList[1]["Class", GffInt], "Name"].get.parseInt.StrRef.tlkText(dlg, tlk)
     result.level2 = classList[1]["ClassLevel", GffShort]
   if classList.len == 3:
-    result.class3 = classes[classList[2]["Class", GffInt], "Name"].get.parseInt.StrRef.tlkText(dlg, tlk)
+    result.class3 = classes2da[classList[2]["Class", GffInt], "Name"].get.parseInt.StrRef.tlkText(dlg, tlk)
     result.level3 = classList[2]["ClassLevel", GffShort]
