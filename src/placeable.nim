8fc5048f0f9194d022d48393621825865de784b4
Hendrik Albers <mail@hendrik.dev>
2020-08-16 01:41:37 +0200

    placeables: palette info

--- src/placeable.nim
+++ src/placeable.nim
@@ -1,3 +1,4 @@
+import tables
 import neverwinter/[gff, resman, tlk]
 import helper
 
@@ -17,10 +18,16 @@ type
     comment: string
 
 proc placeableList*(list: seq[ResRef], rm: ResMan, dlg: SingleTlk, tlk: Option[SingleTlk]): seq[Placeable] =
+  let isMod = rm[newResRef("module", "ifo".getResType)].isSome
+  let palcusInfo = if isMod: rm.getGffRoot("placeablepalcus", "itp")["MAIN", GffList].toPalcusInfo(dlg, tlk) else: PalcusInfo()
   for rr in list:
     let utp = rm.getGffRoot(rr)
+    let paletteId = utp["PaletteID", 0.GffByte].int
     result &= Placeable(
       name: utp["LocName", GffCExoLocString].getStr(dlg, tlk),
       resref: rr.resRef,
       tag: utp["Tag", ""],
+      palette: palcusInfo.getOrDefault(paletteId).name,
+      palette_full: palcusInfo.getOrDefault(paletteId).full,
+      palette_id: paletteId,
     )
