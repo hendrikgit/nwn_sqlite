d02e7e385dd105b1118633a33aba2344bdb9a898
Hendrik Albers <mail@hendrik.dev>
2020-08-15 23:26:12 +0200

    drop table when no entries (better to have no table than old data)

--- src/db.nim
+++ src/db.nim
@@ -7,6 +7,8 @@ proc createTable(db: DbConn, tablename: string, cols: seq[tuple[name, coltype: s
   db.exec(create.sql)
 
 proc writeTable*[T](s: seq[T], filename, tablename: string) =
+  let db = open(filename, "", "", "")
+  db.exec(sql("drop table if exists " & tablename))
   if s.len == 0: return
   var cols = newSeq[tuple[name, coltype: string]]()
   for k, v in s[0].fieldPairs:
@@ -16,8 +18,6 @@ proc writeTable*[T](s: seq[T], filename, tablename: string) =
       cols &= (k, "text")
     when v isnot int | string:
       {.fatal: "Handling of this type not implemented.".}
-  let db = open(filename, "", "", "")
-  db.exec(sql("drop table if exists " & tablename))
   createTable(db, tablename, cols)
   db.exec(sql"begin transaction")
   let insertcols = "insert into " & tablename & " (" & cols.mapIt(it.name).join(",") & ")"
