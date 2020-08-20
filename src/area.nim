5907a5d8718b77435cc3f168fc127a08e4c6ef2a
Hendrik Albers <mail@hendrik.dev>
2020-08-20 22:47:00 +0200

    areas: added columns for sound and music resource names from 2da

--- src/area.nim
+++ src/area.nim
@@ -1,5 +1,5 @@
 import strutils, tables
-import neverwinter/[gff, resman, tlk]
+import neverwinter/[gff, resman, tlk, twoda]
 import helper
 
 type
@@ -22,6 +22,8 @@ type
     comments: string
     ambientSndDay, ambientSndDayVol, ambientSndNight, ambientSndNitVol: int
     envAudio, musicBattle, musicDay, musicDelay, musicNight: int
+    xAmbientSndDayResource, xAmbientSndNightResource: string
+    xMusicBattleResource, xMusicDayResource, xMusicDelayResource, xMusicNightResource: string
 
   AreaFlag {.size: 4.} = enum
     areaInterior = "interior" # exterior if unset
@@ -34,6 +36,8 @@ proc toFlags(v: int): AreaFlags =
   cast[AreaFlags](v)
 
 proc areaList*(list: seq[ResRef], rm: ResMan, dlg, tlk: Option[SingleTlk]): seq[Area] =
+  let sound2da = rm.get2da("ambientsound")
+  let music2da = rm.get2da("ambientmusic")
   for rr in list:
     let
       are = rm.getGffRoot(rr)
@@ -74,4 +78,12 @@ proc areaList*(list: seq[ResRef], rm: ResMan, dlg, tlk: Option[SingleTlk]): seq[
         case label
         of "Tileset", "OnEnter", "OnExit":
           v = $are[label, GffResRef]
+    if sound2da.isSome:
+      area.xAmbientSndDayResource = sound2da.get(TwoDA())[area.ambientSndDay, "Resource", ""]
+      area.xAmbientSndNightResource = sound2da.get[area.ambientSndNight, "Resource", ""]
+    if music2da.isSome:
+      area.xMusicBattleResource = music2da.get[area.musicBattle, "Resource", ""]
+      area.xMusicDayResource = music2da.get[area.musicDay, "Resource", ""]
+      area.xMusicDelayResource = music2da.get[area.musicDelay, "Resource", ""]
+      area.xMusicNightResource = music2da.get[area.musicNight, "Resource", ""]
     result &= area
