2feed4928169d3c8ebd3e6eb89eb767699ca54c7
Hendrik Albers <mail@hendrik.dev>
2020-08-20 18:36:07 +0200

    capitalize first letter, convert x to underscore

--- src/db.nim
+++ src/db.nim
@@ -12,12 +12,13 @@ proc writeTable*[T](s: seq[T], filename, tablename: string) =
   if s.len == 0: return
   var cols = newSeq[tuple[name, coltype: string]]()
   for k, v in s[0].fieldPairs:
+    let name = if k[0] == 'x': '_' & k[1 .. ^1] else: k.capitalizeAscii
     if v is bool | int:
-      cols &= (k, "integer")
+      cols &= (name, "integer")
     if v is float:
-      cols &= (k, "real")
+      cols &= (name, "real")
     elif v is string:
-      cols &= (k, "text")
+      cols &= (name, "text")
     when v isnot bool| float| int | string:
       {.fatal: "Handling of this type not implemented.".}
   createTable(db, tablename, cols)
