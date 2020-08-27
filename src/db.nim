2ddfcc72fb48dd109646a6099438b2310e5dccd6
Hendrik Albers <mail@hendrik.dev>
2020-08-27 23:06:25 +0200

    template (& macro) to write 2da tables, db: id can be given explicitly

--- src/db.nim
+++ src/db.nim
@@ -3,7 +3,7 @@ import db_sqlite, sequtils, strutils
 proc createTable(db: DbConn, tablename: string, cols: seq[tuple[name, coltype: string]]) =
   let create = "create table " & tablename &
     " (id integer primary key," &
-    cols.mapIt(it.name & " " & it.coltype).join(",") & ")"
+    cols.filterIt(it.name != "id").mapIt(it.name & " " & it.coltype).join(",") & ")"
   db.exec(create.sql)
 
 proc writeTable*[T](s: seq[T], filename, tablename: string) =
@@ -12,13 +12,17 @@ proc writeTable*[T](s: seq[T], filename, tablename: string) =
   if s.len == 0: return
   var cols = newSeq[tuple[name, coltype: string]]()
   for k, v in s[0].fieldPairs:
-    let name = if k[0] == 'x': '_' & k[1 .. ^1] else: k.capitalizeAscii
-    if v is bool | int:
+    let name = if k == "id": k
+      elif k[0] == 'x': '_' & k[1 .. ^1]
+      else: k.capitalizeAscii
+    when v is bool | int:
       cols &= (name, "integer")
-    if v is float:
+    when v is float:
       cols &= (name, "real")
-    elif v is string:
+    when v is string:
       cols &= (name, "text")
+    when k == "id" and v isnot int:
+      {.fatal: "Column \"id\" has to be of type int.".}
     when v isnot bool | float | int | string:
       {.fatal: "Handling of this type not implemented.".}
   createTable(db, tablename, cols)
---
+++ src/restables.nim
@@ -0,0 +1,39 @@
+import macros, sequtils, strutils, tables
+import neverwinter/[resman, twoda]
+import db
+
+macro tupleDef(cols: openArray[string]): untyped =
+  # cols.getImpl.treeRepr:
+  # IdentDefs
+  #   Sym "cols"
+  #   Empty
+  #   Bracket
+  #     StrLit "x"
+  #     StrLit "y"
+  #     ...
+  let colStrings = toSeq(cols.getImpl[2].children).mapIt it.strVal
+  parseStmt("type TwodaRow = tuple[id:int," & colStrings.join(",") & ":string]")
+
+template write2daTable*(rm: ResMan, dbName, twodaName, tableName: string, cols: openArray[string]) =
+  let resref = newResRef(twodaName, "2da".getResType)
+  if rm.contains(resref):
+    let twoda = rm[resref].get.readAll.newStringStream.readTwoDA
+    var colIdxs = initTable[string, int]()
+    for idx, c in twoda.columns:
+      if cols.findIt(it == c).isSome:
+        colIdxs[c] = idx
+    let colsForMacro = cols # the macro needs symbols to work on and cols passed to this template might be an automatic variable
+    tupleDef(colsForMacro)
+    var rows = newSeq[TwodaRow]()
+    for rowIdx in 0 .. twoda.high:
+      var add = false
+      var row: TwodaRow
+      for col, v in row.fieldPairs:
+        when col != "id":
+          if twoda[rowIdx].get[colIdxs[col]].isSome:
+            add = true
+            v = twoda[rowIdx].get[colIdxs[col]].get
+      if add:
+        row.id = rowIdx
+        rows &= row
+    rows.writeTable(dbName, tableName)
