d217f0887b6caf23399b9b1429066e952bcda1ab
Hendrik Albers <mail@hendrik.dev>
2020-09-28 18:56:51 +0200

    db: refactor, no functional changes

--- src/db.nim
+++ src/db.nim
@@ -1,40 +1,57 @@
 import db_sqlite, sequtils, strutils
 
-proc createTable(db: DbConn, tablename: string, cols: seq[tuple[name, coltype: string]]) =
+type
+  SqliteType = enum
+    sqliteInteger = "integer"
+    sqliteReal = "real"
+    sqliteText = "text"
+
+  Column = tuple
+    name: string
+    coltype: SqliteType
+
+proc createTable(db: DbConn, tablename: string, cols: seq[Column]) =
   let create = "create table " & tablename &
     " (id integer primary key," &
-    cols.filterIt(it.name != "id").mapIt(it.name & " " & it.coltype).join(",") & ")"
+    cols.filterIt(it.name != "id").mapIt(it.name & " " & $it.coltype).join(",") & ")"
   db.exec(create.sql)
 
-proc writeTable*[T](s: seq[T], filename, tablename: string) =
+proc insert(db: DbConn, tablename: string, cols: seq[Column], rows: seq[seq[string]]) =
+  let insertcols = "insert into " & tablename & " (" & cols.mapIt(it.name).join(",") & ")"
+  let rowLen = cols.len # all rows have to have the same length, it would be an error otherwise
+  db.exec(sql"begin transaction")
+  for row in rows:
+    db.exec(sql(insertcols & " values (" & newSeqWith(rowLen, "?").join(",") & ")"), row)
+  db.exec(sql"commit")
+
+proc writeTable*(rows: seq[object | tuple], filename, tablename: string) =
   let db = open(filename, "", "", "")
   db.exec(sql("drop table if exists " & tablename))
-  if s.len == 0: return
-  var cols = newSeq[tuple[name, coltype: string]]()
-  for k, v in s[0].fieldPairs:
+  if rows.len == 0: return
+  var cols = newSeq[Column]()
+  for k, v in rows[0].fieldPairs:
     let name = if k == "id": k
       elif k[0] == 'x': '_' & k[1 .. ^1]
       else: k.capitalizeAscii
     when v is bool | int:
-      cols &= (name, "integer")
+      cols &= (name, sqliteInteger)
     when v is float:
-      cols &= (name, "real")
+      cols &= (name, sqliteReal)
     when v is string:
-      cols &= (name, "text")
+      cols &= (name, sqliteText)
     when k == "id" and v isnot int:
       {.fatal: "Column \"id\" has to be of type int.".}
     when v isnot bool | float | int | string:
       {.fatal: "Handling of this type not implemented.".}
-  createTable(db, tablename, cols)
-  db.exec(sql"begin transaction")
-  let insertcols = "insert into " & tablename & " (" & cols.mapIt(it.name).join(",") & ")"
-  for el in s:
-    var values = newSeq[string]()
-    for _, v in el.fieldPairs:
+  db.createTable(tablename, cols)
+  var stringRows = newSeq[seq[string]]()
+  for row in rows:
+    var stringRow = newSeq[string]()
+    for _, v in row.fieldPairs:
       when v is bool:
-        values &= $v.int
+        stringRow &= $v.int
       else:
-        values &= $v
-    db.exec(sql(insertcols & " values (" & newSeqWith(values.len, "?").join(",") & ")"), values)
-  db.exec(sql"commit")
+        stringRow &= $v
+    stringRows &= stringRow
+  db.insert(tablename, cols, stringRows)
   db.close
