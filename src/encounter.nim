c48ff34049d6dcb6f54a1673981e67aa29125a0d
Hendrik Albers <mail@hendrik.dev>
2020-09-29 18:48:14 +0200

    enum for field type

--- src/encounter.nim
+++ src/encounter.nim
@@ -1,39 +1,50 @@
 import neverwinter/[gff, resman, tlk]
 import db, helper
 
-const fields = [
-  ["id", "id"],
-  ["Active", "byte"],
-  ["Comment", "cexostring"],
-  ["Difficulty", "int"],
-  ["DifficultyIndex", "int"],
-  ["Faction", "dword"],
-  ["LocalizedName", "cexolocstring"],
-  ["MaxCreatures", "int"],
-  ["OnEntered", "resref"],
-  ["OnExhausted", "resref"],
-  ["OnExit", "resref"],
-  ["OnHeartbeat", "resref"],
-  ["OnUserDefined", "resref"],
-  ["PaletteID", "byte"],
-  ["PlayerOnly", "byte"],
-  ["RecCreatures", "int"],
-  ["Reset", "byte"],
-  ["ResetTime", "int"],
-  ["Respawns", "int"],
-  ["SpawnOption", "int"],
-  ["Tag", "cexostring"],
-  ["TemplateResRef", "resref"],
+type
+  FieldType = enum
+    ftId
+    ftByte, ftDword, ftInt
+    ftFloat
+    ftResRef, ftCExoString,
+    ftCExoLocString
+
+  Field = tuple
+    name: string
+    fieldType: FieldType
+
+const fields: array[22, Field] = [
+  ("id", ftId),
+  ("Active", ftByte),
+  ("Comment", ftCExoString),
+  ("Difficulty", ftInt),
+  ("DifficultyIndex", ftInt),
+  ("Faction", ftDword),
+  ("LocalizedName", ftCExoLocString),
+  ("MaxCreatures", ftInt),
+  ("OnEntered", ftResRef),
+  ("OnExhausted", ftResRef),
+  ("OnExit", ftResRef),
+  ("OnHeartbeat", ftResRef),
+  ("OnUserDefined", ftResRef),
+  ("PaletteID", ftByte),
+  ("PlayerOnly", ftByte),
+  ("RecCreatures", ftInt),
+  ("Reset", ftByte),
+  ("ResetTime", ftInt),
+  ("Respawns", ftInt),
+  ("SpawnOption", ftInt),
+  ("Tag", ftCExoString),
+  ("TemplateResRef", ftResRef),
 ]
 
-# todo: can this be done at compile time?
 proc encounterCols(): seq[Column] =
   for f in fields:
-    let coltype = case f[1]
-      of "id", "byte", "dword", "int": sqliteInteger
-      of "float": sqliteReal
-      else: sqliteText
-    result &= (name: f[0], coltype: coltype)
+    let coltype = case f.fieldType
+      of ftId, ftByte, ftDword, ftInt: sqliteInteger
+      of ftFloat: sqliteReal
+      of ftResRef, ftCExoString, ftCExoLocString: sqliteText
+    result &= (name: f.name, coltype: coltype)
 
 # todo: add looked up names for faction and parent faction
 proc writeEncounterTables*(list: seq[ResRef], rm: ResMan, dlg, tlk: Option[SingleTlk], dbName: string) =
@@ -44,17 +55,15 @@ proc writeEncounterTables*(list: seq[ResRef], rm: ResMan, dlg, tlk: Option[Singl
     let ute = rm.getGffRoot(rr)
     var row = newSeq[string]()
     for f in fields:
-      row.add case f[1]
-      of "id": id # id is added "manually" just in case sqlite does not number the rows as expected
-      of "byte": $ute[f[0], 0.GffByte]
-      of "cexolocstring": ute[f[0], GffCExoLocString].getStr(dlg, tlk)
-      of "cexostring": ute[f[0], "".GffCExoString]
-      of "dword": $ute[f[0], 0.GffDword]
-      of "int": $ute[f[0], -1.GffInt]
-      of "resref": $ute[f[0], "".GffResRef]
-      else:
-        echo "Error: Handling of field type " & f[1] & " not implemented."
-        quit(QuitFailure)
+      row.add case f.fieldType
+      of ftId: id # id is added "manually" just in case sqlite does not number the rows as expected
+      of ftByte: $ute[f.name, 0.GffByte]
+      of ftDword: $ute[f.name, 0.GffDword]
+      of ftInt: $ute[f.name, -1.GffInt]
+      of ftFloat: $ute[f.name, -1.GffFloat]
+      of ftResRef: $ute[f.name, "".GffResRef]
+      of ftCExoString: ute[f.name, "".GffCExoString]
+      of ftCExoLocString: ute[f.name, GffCExoLocString].getStr(dlg, tlk)
     encounters &= row
     for c in ute["CreatureList", GffList]:
       creatures &= @[id, $c["ResRef", "".GffResRef], $c["SingleSpawn", 0.GffByte]]
